d021bf43c88784a90b25161f7ecf9964c0a2b607
alvaromb <xlasttrainhomex@gmail.com>
2014-11-20 17:27:48 +0100

    Added support to Launch Images

--- generator.py
+++ generator.py
@@ -9,9 +9,9 @@ import argparse
 from PIL import Image
 
 
-def main():
+def generate_icon_images(icon_filename):
     sizes = [29, 40, 50, 57, 58, 72, 76, 80, 87, 100, 114, 120, 144, 152, 180]
-    image_name = "icon.png"
+    image_name = icon_filename
     contents = {
         "info": {
             "version": 1,
@@ -20,9 +20,13 @@ def main():
     }
 
     if os.path.isfile(image_name) is False:
-        sys.exit("Missing icon.png file in current directory")
+        sys.exit("Missing {0} file".format(image_name))
 
     img = Image.open(image_name)
+
+    # Check image size
+    if img.size < (1024, 1024):
+        sys.exit("{0} size is {1}. Must be 1024x1024 or higher.".format(image_name, img.size))
 
     # Create Assets folder
     current_dir = os.getcwd()
@@ -193,5 +197,124 @@ def main():
     file.close()
 
 
+def generate_launch_image(image_name):
+    # 1334x750 not available yet, see: http://stackoverflow.com/a/26275887/1034126
+    sizes = [(1242, 2208), (2208, 1242), (750, 1334), (640, 1136), (640, 960)]
+    contents = {
+        "info": {
+            "version": 1,
+            "author": "apsl"
+        }
+    }
+
+    if os.path.isfile(image_name) is False:
+        sys.exit("Missing {0} file".format(image_name))
+
+    img = Image.open(image_name)
+
+    # Check image size
+    if img.size < (1242, 2208):
+        sys.exit("{0} size is {1}. Must be 1242x2208 or higher.".format(image_name, img.size))
+
+    # Create Assets folder
+    current_dir = os.getcwd()
+    asset_dir = "{0}/LaunchImage.launchimage".format(current_dir)
+    if not os.path.exists(asset_dir):
+        os.makedirs(asset_dir)
+    os.chdir(asset_dir)
+    print asset_dir
+
+    # Generate images
+    images = []
+    for size in sizes:
+        i = img.resize(size, Image.ANTIALIAS)
+        img_filename = "launchimage{0}x{1}.png".format(size[0], size[1])
+        i.save(img_filename, format="PNG")
+        if size == (1242, 2208):
+            asset_entry = {
+                "extent": "full-screen",
+                "idiom": "iphone",
+                "subtype": "736h",
+                "minimum-system-version": "8.0",
+                "orientation": "portrait",
+                "scale": "3x",
+                "filename": img_filename
+            }
+        elif size == (2208, 1242):
+            asset_entry = {
+                "orientation": "landscape",
+                "idiom": "iphone",
+                "extent": "full-screen",
+                "minimum-system-version": "8.0",
+                "subtype": "736h",
+                "scale": "3x",
+                "filename": img_filename
+            }
+        elif size == (750, 1334):
+            asset_entry = {
+                "extent": "full-screen",
+                "idiom": "iphone",
+                "subtype": "667h",
+                "minimum-system-version": "8.0",
+                "orientation": "portrait",
+                "scale": "2x",
+                "filename": img_filename
+            }
+        elif size == (640, 1136):
+            asset_entry = {
+                "orientation": "portrait",
+                "idiom": "iphone",
+                "extent": "full-screen",
+                "minimum-system-version": "7.0",
+                "subtype": "retina4",
+                "scale": "2x",
+                "filename": img_filename
+            }
+            images.append(asset_entry)
+            asset_entry = {
+                "orientation": "portrait",
+                "idiom": "iphone",
+                "extent": "full-screen",
+                "subtype": "retina4",
+                "scale": "2x",
+                "filename": img_filename
+            }
+        elif size == (640, 960):
+            asset_entry = {
+                "orientation": "portrait",
+                "idiom": "iphone",
+                "extent": "full-screen",
+                "minimum-system-version": "7.0",
+                "scale": "2x",
+                "filename": img_filename
+            }
+            images.append(asset_entry)
+            asset_entry = {
+                "orientation": "portrait",
+                "idiom": "iphone",
+                "extent": "full-screen",
+                "scale": "2x",
+                "filename": img_filename
+            }
+        images.append(asset_entry)
+
+    contents["images"] = images
+    file = open("Contents.json", "w")
+    file.write(json.dumps(contents))
+    file.close()
+
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Assets generator for iOS projects")
+    parser.add_argument("--launchimage",
+                        help="Generate launch image assets")
+    parser.add_argument("--icon",
+                        help="Generate icons assets")
+    parser.parse_args()
+    args = parser.parse_args()
+    if args.launchimage:
+        print "Generating launch image with image {0}".format(args.launchimage)
+        generate_launch_image(args.launchimage)
+    if args.icon:
+        print "Generating icons with image {0}".format(args.icon)
+        generate_icon_images(args.icon)
